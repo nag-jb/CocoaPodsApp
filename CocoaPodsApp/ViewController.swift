2cddbb9750c232771067c73df729585c4998ac21
Nagiko Tatsui <n.t.woody.0914@gmail.com>
2021-02-13 20:05:11 +0900

    CocoaPodsアプリの完成_GitHub

--- CocoaPodsApp/ViewController.swift
+++ CocoaPodsApp/ViewController.swift
@@ -6,12 +6,24 @@
 //
 
 import UIKit
+//CocoaPodsで追加したPKHUDライブラリのimport(読み込み)
+import PKHUD
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func viewDidAppear(_ animated: Bool){
+        super.viewDidAppear(animated)
+        //viewDidAppearのタイミングでHUDの表示
+        HUD.flash(.success, delay: 2.0)
     }
 
 
